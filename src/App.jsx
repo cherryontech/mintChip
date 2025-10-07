48ac77136cd2b3a34289cf00e48446b476a4e18f
CMolinaBetancourt <cathemolinab@gmail.com>
2025-10-07 10:51:01 -0700

    feat: setup project and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
+    <div className="min-h-screen bg-emerald-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-lime-500">
         Mint Chips - Tailwind works! 🎉
       </h1>
     </div>
